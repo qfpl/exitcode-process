dc0f2c1fd642ebb7169f42ab7ab262a4fdfbf5ee
Dave Laing <dave.laing.80@gmail.com>
2018-11-22 10:14:21 +1000

    Bumps some version

--- default.nix
+++ default.nix
@@ -5,30 +5,22 @@ let
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  tasty-hedgehog-github = pkgs.callPackage (pkgs.fetchFromGitHub {
-    owner = "qfpl";
-    repo = "tasty-hedgehog";
-    rev = "5da389f5534943b430300a213c5ffb5d0e13459e";
-    sha256 = "04pmr9q70gakd327sywpxr7qp8jnl3b0y2sqxxxcj6zj2q45q38m";
-  }) {};
-
   sources = {
 
     exitcode = (pkgs.fetchFromGitHub {
       owner = "qfpl";
       repo = "exitcode";
-      rev = "dc2f8e44d8e918dd20636e4a65bde7dd3ff73ae8";
-      sha256 = "1llynvjdhiz19s8ax1nzzmpq4xg6s76xlz5yglyszqw8zqc12p24";
+      rev = "bdcd9f3ed7db539163eb7b3d9bd0c27e543163d7";
+      sha256 = "1pai4x3q82z2nkc7cqv8q15n2n71mam4g357ix1g0ma9zpq8mfjw";
     });
 
   };
 
   modifiedHaskellPackages = haskellPackages.override {
     overrides = self: super: {
-      tasty-hedgehog =
-        if super ? tasty-hedgehog
-        then super.tasty-hedgehog
-        else tasty-hedgehog-github;
+      hedgehog       = self.callHackage "hedgehog" "0.6" {};
+      tasty-hedgehog = self.callHackage "tasty-hedgehog" "0.2.0.0" {};
+      polyparse = self.callHackage "polyparse" "1.12.1" {};
       exitcode = super.callPackage sources.exitcode {};
     };
   };
