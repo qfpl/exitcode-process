c868c9d25f9d2da33898ed5e301e50842f6a3331
Dave Laing <dave.laing.80@gmail.com>
2018-11-22 10:38:24 +1000

    Tries to get this to build on Hydra

--- default.nix
+++ default.nix
@@ -10,18 +10,19 @@ let
     exitcode = (pkgs.fetchFromGitHub {
       owner = "qfpl";
       repo = "exitcode";
-      rev = "bdcd9f3ed7db539163eb7b3d9bd0c27e543163d7";
-      sha256 = "1pai4x3q82z2nkc7cqv8q15n2n71mam4g357ix1g0ma9zpq8mfjw";
+      rev = "bc31d0de9bd4886b912e03bdfa4a6c262b35fbf8";
+      sha256 = "08vi4j77vfv6k1dnwfrvvrg9dzijk3c7iynw9k5d6wxb5chk9xhy";
     });
 
   };
 
   modifiedHaskellPackages = haskellPackages.override {
     overrides = self: super: {
-      # hedgehog       = self.callHackage "hedgehog" "0.6" {};
-      # tasty-hedgehog = self.callHackage "tasty-hedgehog" "0.2.0.0" {};
-      # polyparse = self.callHackage "polyparse" "1.12.1" {};
-      exitcode = super.callPackage sources.exitcode {};
+      hedgehog       = self.callHackage "hedgehog" "0.6" {};
+      tasty-hedgehog = self.callHackage "tasty-hedgehog" "0.2.0.0" {};
+      polyparse = self.callHackage "polyparse" "1.12.1" {};
+      concurrent-output = pkgs.haskell.lib.doJailbreak super.concurrent-output;
+      exitcode = super.callPackage sources.exitcode { inherit nixpkgs compiler; };
     };
   };
 
